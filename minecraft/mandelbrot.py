ae775782233a5618cdcba456a3643859d80211a3
Robin Smith <robin@mrrobinsmith.com>
2015-02-22 19:06:20 +0000

    Auto-update.

--- minecraft/mandelbrot.py
+++ minecraft/mandelbrot.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-#!/usr/bin/env python
-
 import sys, random
-# sys.path.append("/home/pi/mcpi/api/python/mcpi")
-# import minecraft
+sys.path.append("/home/pi/mcpi/api/python/mcpi")
+import minecraft
 
 from complex import Complex
 
@@ -15,12 +13,12 @@ FOREGROUND_BLOCK = 49
 HEIGHT = 100
 WIDTH = HEIGHT * 7 / 4
 
-ITERATIONS=1000
-CONSTANT=-1
+ITERATIONS=10
+CONSTANT = Complex(1, 1)
 
-# mc = minecraft.Minecraft.create()
+mc = minecraft.Minecraft.create()
 #mc.postToChat("Welcome to minecraft Maze!")
-# mc.player.setTilePos(0, 25, 0)
+mc.player.setTilePos(WIDTH / 2, 10, HEIGHT / 2)
 
 x_min = -2.5
 x_max = 1
@@ -35,21 +33,10 @@ dy = y_range / HEIGHT
 # mc.setBlocks(-WIDTH / 2, 20, -HEIGHT / 2, WIDTH / 2, 20, HEIGHT /2, BACKGROUND_BLOCK)
 
 
-# Do the calculation
-points = []
-complex = Complex(0, 0);
-points.append(complex)
+points = [[0] * WIDTH] * WIDTH
 
-for i in range(1, ITERATIONS):
-    complex = Complex(complex.x - CONSTANT, complex.y).square()
-    points.append(complex)
-
-
-
-# Plot the points
-for point in points:
-    print point.x
-    print point.y
-    # mc.setBlock(point.x, 20, point.y, FOREGROUND_BLOCK)
-
-
+for x in [0.1 * x for x in range(int(x_min * 10), int(x_max +1 * 10))]:
+    for y in [0.1 * y for y in range(int(y_min * 10), int(y_max +1 * 10))]:
+       square = x * x - y * x + 2 * x * y 
+       if square < 4:
+           mc.setBlock(int(x * WIDTH), GROUND, int(y * HEIGHT), FOREGROUND_BLOCK)
