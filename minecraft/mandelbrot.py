0b474fc834400bc19d58fb35939a0e928488b659
Toby Smith <toby.smith3@ntlworld.com>
2015-02-22 19:53:24 +0000

    Update

--- minecraft/mandelbrot.py
+++ minecraft/mandelbrot.py
@@ -10,33 +10,41 @@ from complex import Complex
 BACKGROUND_BLOCK = 155
 FOREGROUND_BLOCK = 49
 
-HEIGHT = 100
-WIDTH = HEIGHT * 7 / 4
+
+GROUND=50
 
 ITERATIONS=10
 CONSTANT = Complex(1, 1)
 
-mc = minecraft.Minecraft.create()
-#mc.postToChat("Welcome to minecraft Maze!")
-mc.player.setTilePos(WIDTH / 2, 10, HEIGHT / 2)
+
+scale = 100
+resolution = 1.0 / scale
 
 x_min = -2.5
 x_max = 1
 x_range = (x_max - x_min)
-dx = x_range / WIDTH
 
 y_min = -1
 y_max = 1
 y_range = (y_max - y_min)
-dy = y_range / HEIGHT
 
-# mc.setBlocks(-WIDTH / 2, 20, -HEIGHT / 2, WIDTH / 2, 20, HEIGHT /2, BACKGROUND_BLOCK)
+WIDTH = int(x_range * scale)
+HEIGHT = int(y_range * scale)
 
 
-points = [[0] * WIDTH] * WIDTH
+mc = minecraft.Minecraft.create()
+#mc.postToChat("Welcome to minecraft Maze!")
 
-for x in [0.1 * x for x in range(int(x_min * 10), int(x_max +1 * 10))]:
-    for y in [0.1 * y for y in range(int(y_min * 10), int(y_max +1 * 10))]:
-       square = x * x - y * x + 2 * x * y 
+mc.setBlocks(-100, -100, -100, 100, 100, 100, 0)
+mc.setBlocks(-5 / 7 * WIDTH, GROUND, -HEIGHT / 2, 2 / 7 * WIDTH, GROUND, HEIGHT / 2, BACKGROUND_BLOCK)
+
+mc.player.setTilePos(0, GROUND+5, 0)
+
+points = [[0] * HEIGHT] * WIDTH
+
+for x in [resolution * x for x in range(int(x_min * scale), int(x_max +1 * scale))]:
+    for y in [resolution * y for y in range(int(y_min * scale), int(y_max +1 * scale))]:
+       square = x * x + y * y
        if square < 4:
-           mc.setBlock(int(x * WIDTH), GROUND, int(y * HEIGHT), FOREGROUND_BLOCK)
+            pass
+            #mc.setBlock(int(x * scale), GROUND, int(y * scale), FOREGROUND_BLOCK)
